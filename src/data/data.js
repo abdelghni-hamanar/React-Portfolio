974b1386f38dd8b2e2d32c9727bd3d4c3bc84adf
abdelghni-hamanar <abdelghni.hamanar@gmail.com>
2022-11-03 15:11:23 +0000

    React Portfolio

---
+++ src/data/data.js
@@ -0,0 +1,29 @@
+import WorkIm from '../assets/projects/workImg.jpeg'
+import RealEst from '../assets/projects/realestate.jpg'
+import Medical from '../assets/projects/Medical.JPG'
+
+export const data=[
+    {
+        id:1,
+        name:"React Portfolio",
+        image:WorkIm,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"Medical File Management",
+        image:Medical,
+        github:"https://github.com/abdelghni-hamanar/medical-file-management",
+        live:"http://abd0.eb2a.com/",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:RealEst,
+        github:"",
+        live:"",
+    },
+
+
+]
